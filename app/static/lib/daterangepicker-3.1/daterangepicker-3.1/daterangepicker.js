37de82413e87ed517ba0cb7adc51275aeae5a28b
Chrono Lux <m.sanma123@gmail.com>
2021-02-04 17:24:55 -0600

    daterangepicker change

--- app/static/lib/daterangepicker-3.1/daterangepicker-3.1/daterangepicker.js
+++ app/static/lib/daterangepicker-3.1/daterangepicker-3.1/daterangepicker.js
@@ -66,7 +66,7 @@
 
         this.buttonClasses = 'btn btn-sm';
         this.applyButtonClasses = 'btn-primary';
-        this.cancelButtonClasses = 'btn-default';
+        this.cancelButtonClasses = 'btn-secondary';
 
         this.locale = {
             direction: 'ltr',
