d35733b4e8ac112f59024f0a1d46fe19f6eea1c3
Chrono Lux <m.sanma123@gmail.com>
2021-02-05 15:53:04 -0600

    report in pdf's and excel

--- app/core/reports/views.py
+++ app/core/reports/views.py
@@ -6,7 +6,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 from core.reports.forms import ReportForm
 from core.erp.models import Sale
-
+from django.db.models.functions import Coalesce
+from django.db.models import Sum
 
 class ReportSaleView(TemplateView):
     template_name = 'sale/report.html'
@@ -36,6 +37,18 @@ class ReportSaleView(TemplateView):
                         format(s.iva, '.2f'),
                         format(s.total, '.2f'),
                     ])
+                subtotal = search.aggregate(r=Coalesce(Sum('subtotal'), 0)).get('r')
+                iva = search.aggregate(r=Coalesce(Sum('iva'), 0)).get('r')
+                total = search.aggregate(r=Coalesce(Sum('total'), 0)).get('r')
+
+                data.append([
+                    '---',
+                    '---',
+                    '---',
+                    format(subtotal, '.2f'),
+                    format(iva, '.2f'),
+                    format(total, '.2f'),
+                ])
             else:
                 data['error'] = 'Ha ocurrido un error'
         except Exception as e:
