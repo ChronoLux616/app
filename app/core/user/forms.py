26f595db035ac7f7458100cecc6344c166c44b4b
Chrono Lux <m.sanma123@gmail.com>
2021-02-10 17:05:22 -0600

    form new user

---
+++ app/core/user/forms.py
@@ -0,0 +1,55 @@
+from django.forms import *
+from core.user.models import User
+
+
+class UserForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # for form in self.visible_fields():
+        #     form.field.widget.attrs['class'] = 'form-control'
+        #     form.field.widget.attrs['autocomplete'] = 'off'
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image'
+        widgets = {
+            'first_name': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su nombre',
+                }
+            ),
+            'last_name': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su apellido',
+                }
+            ),
+            'email': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su correo electronico',
+                }
+            ),
+            'username': TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su nick o username',
+                }
+            ),
+            'password': PasswordInput(
+                attrs={
+                    'placeholder': 'Ingrese su contraseña',
+                }
+            ),
+        }
+        exclude = ['groups', 'user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff']
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                form.save()
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
