2fb5fd63112e25704566b9d80121ba3264c48c4f
Chrono Lux <m.sanma123@gmail.com>
2021-02-17 20:44:52 -0600

    session

--- app/core/erp/mixins.py
+++ app/core/erp/mixins.py
@@ -38,3 +38,20 @@ class ValidatePermissionRequiredMixin(object):
             return super().dispatch(request, *args, **kwargs)
         messages.error(request, 'No tiene permiso para ingresar a este módulo')
         return HttpResponseRedirect(self.get_url_redirect())
+
+
+# class ValidatePermissionRequiredMixin(object):
+#     permission_required = ''
+#     url_redirect = None
+#
+#     def get_url_redirect(self):
+#         if self.url_redirect is None:
+#             return reverse_lazy('erp:dashboard')
+#         return self.url_redirect
+#
+#     def dispatch(self, request, *args, **kwargs):
+#
+#         if request.user.has_perms(self.get_perms()):
+#             return super().dispatch(request, *args, **kwargs)
+#         messages.error(request, 'No tiene permiso para ingresar a este módulo')
+#         return HttpResponseRedirect(self.get_url_redirect())
