56d75c4d4459855fd8803bab49bb049505cc6b95
Chrono Lux <m.sanma123@gmail.com>
2021-02-21 21:46:08 -0600

    user profile form

--- app/core/user/forms.py
+++ app/core/user/forms.py
@@ -1,4 +1,5 @@
-from django.forms import *
+from django import forms
+from django.forms import ModelForm
 from core.user.models import User
 
 
@@ -14,32 +15,32 @@ class UserForm(ModelForm):
         model = User
         fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image', 'groups'
         widgets = {
-            'first_name': TextInput(
+            'first_name': forms.TextInput(
                 attrs={
                     'placeholder': 'Ingrese sus nombres',
                 }
             ),
-            'last_name': TextInput(
+            'last_name': forms.TextInput(
                 attrs={
                     'placeholder': 'Ingrese sus apellidos',
                 }
             ),
-            'email': TextInput(
+            'email': forms.TextInput(
                 attrs={
                     'placeholder': 'Ingrese su email',
                 }
             ),
-            'username': TextInput(
+            'username': forms.TextInput(
                 attrs={
                     'placeholder': 'Ingrese su username',
                 }
             ),
-            'password': PasswordInput(render_value=True,
+            'password': forms.PasswordInput(render_value=True,
                                       attrs={
                                           'placeholder': 'Ingrese su password',
                                       }
                                       ),
-            'groups': SelectMultiple(attrs={
+            'groups': forms.SelectMultiple(attrs={
                 'class': 'form-control select2',
                 'style': 'width: 100%',
                 'multiple': 'multiple'
@@ -65,6 +66,67 @@ class UserForm(ModelForm):
 
                 for g in self.cleaned_data['groups']:
                     u.groups.add(g)
+            else:
+                data['error'] = form.errors
+        except Exception as e:
+            data['error'] = str(e)
+        return data
+
+
+class UserProfileForm(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # for form in self.visible_fields():
+        #     form.field.widget.attrs['class'] = 'form-control'
+        #     form.field.widget.attrs['autocomplete'] = 'off'
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+
+    class Meta:
+        model = User
+        fields = 'first_name', 'last_name', 'email', 'username', 'password', 'image'
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ingrese sus nombres',
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ingrese sus apellidos',
+                }
+            ),
+            'email': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su email',
+                }
+            ),
+            'username': forms.TextInput(
+                attrs={
+                    'placeholder': 'Ingrese su username',
+                }
+            ),
+            'password': forms.PasswordInput(render_value=True,
+                                      attrs={
+                                          'placeholder': 'Ingrese su password',
+                                      }
+                                      ),
+        }
+        exclude = ['user_permissions', 'last_login', 'date_joined', 'is_superuser', 'is_active', 'is_staff', 'groups']
+
+    def save(self, commit=True):
+        data = {}
+        form = super()
+        try:
+            if form.is_valid():
+                pwd = self.cleaned_data['password']
+                u = form.save(commit=False)
+                if u.pk is None:
+                    u.set_password(pwd)
+                else:
+                    user = User.objects.get(pk=u.pk)
+                    if user.password != pwd:
+                        u.set_password(pwd)
+                u.save()
             else:
                 data['error'] = form.errors
         except Exception as e:
