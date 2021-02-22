5a8e89458a82093ff68995b59f490c45838e22f6
Chrono Lux <m.sanma123@gmail.com>
2021-02-21 19:46:18 -0600

    users permissions

--- app/core/erp/mixins.py
+++ app/core/erp/mixins.py
@@ -4,7 +4,7 @@ from django.contrib import messages
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from crum import get_current_request
-from django.contrib.auth.models import Group
+
 
 class IsSuperuserMixin(object):
     def dispatch(self, request, *args, **kwargs):
@@ -45,6 +45,14 @@ class ValidatePermissionRequiredMixin(object):
     permission_required = ''
     url_redirect = None
 
+    def get_perms(self):
+        perms = []
+        if isinstance(self.permission_required, str):
+            perms.append(self.permission_required)
+        else:
+            perms = list(self.permission_required)
+        return perms
+
     def get_url_redirect(self):
         if self.url_redirect is None:
             return reverse_lazy('erp:dashboard')
@@ -52,10 +60,15 @@ class ValidatePermissionRequiredMixin(object):
 
     def dispatch(self, request, *args, **kwargs):
         request = get_current_request()
+        if request.user.is_superuser:
+            return super().dispatch(request, *args, **kwargs)
         if 'group' in request.session:
             group = request.session['group']
-            #group = Group.objects.get(pk=1)
-            if group.permissions.filter(codename=self.permission_required):
-                return super().dispatch(request, *args, **kwargs)
+            perms = self.get_perms()
+            for p in perms:
+                if not group.permissions.filter(codename=p).exists():
+                    messages.error(request, 'No tiene permiso para ingresar a este módulo')
+                    return HttpResponseRedirect(self.get_url_redirect())
+            return super().dispatch(request, *args, **kwargs)
         messages.error(request, 'No tiene permiso para ingresar a este módulo')
         return HttpResponseRedirect(self.get_url_redirect())
