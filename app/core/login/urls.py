804a4990e6474f6ac9c47fb5d60fa40a446dfb32
Chrono Lux <m.sanma123@gmail.com>
2021-02-26 19:14:14 -0600

    url reset password

--- app/core/login/urls.py
+++ app/core/login/urls.py
@@ -4,5 +4,6 @@ from core.login.views import *
 urlpatterns = [
     path('', LoginFormView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('reset/password/', ResetPasswordView.as_view(), name='reset_password'),
     # path('logout/', LogoutRedirectView.as_view(), name='logout'),
 ]
