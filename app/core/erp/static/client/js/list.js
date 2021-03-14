2c9dc54bcd9579dd68152000fb2c3dbf3ee4a5eb
Chrono Lux <m.sanma123@gmail.com>
2021-03-14 16:16:31 -0600

    changes client

--- app/core/erp/static/client/js/list.js
+++ app/core/erp/static/client/js/list.js
@@ -13,7 +13,7 @@ $(function () {
             dataSrc: ""
         },
         columns: [
-            {"data": "id"},
+            {"data": "pos"},
             {"data": "names"},
             {"data": "surnames"},
             {"data": "dni"},
