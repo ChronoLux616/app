ae09c92898244c09b4b4a55659f5ac508e38e0db
Chrono Lux <m.sanma123@gmail.com>
2021-03-20 23:50:24 -0600

    email_change

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,12 +1,16 @@
+from config.wsgi import *
 import smtplib
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from django.template.loader import render_to_string
+from core.user.models import User
 
 
-EMAIL_HOST = 'smtp.xx.com'
+EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'email'
 EMAIL_HOST_PASSWORD = 'password'
-EMAIL_TO = 'addressee'
+EMAIL_TO = 'address'
 
 
 def send_email():
@@ -18,11 +22,13 @@ def send_email():
         mailServer.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
         print('Conectado...')
 
-        mensaje = MIMEText(""" Este es un mensaje de Prueba :)""")
+        mensaje = MIMEMultipart(""" Este es un nuevo mensaje de Prueba""")
         mensaje['From'] = EMAIL_HOST_USER
         mensaje['To'] = EMAIL_TO
         mensaje['Subject'] = "Tienes un nuevo correo"
 
+        content = render_to_string('send_email.html', {'user': User.objects.get(pk=1)})
+        mensaje.attach(MIMEText(content, 'html'))
         mailServer.sendmail(EMAIL_HOST_USER, EMAIL_TO, mensaje.as_string())
 
         print("Correo enviado")
